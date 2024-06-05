5baa249370bb6d38519a09adfaa53a46191ce6f0
SBerti <samuelebrt4@gmail.com>
2024-06-05 17:56:42 +0200

    Add files via upload

---
+++ Manipolazione DOM - 1/index.js
@@ -0,0 +1,11 @@
+const addProduct = () => {
+  const input = document.getElementsByTagName("input")[0];
+  const inputValue = input.value;
+  const li = document.createElement("li");
+  li.innerText = inputValue;
+  const input1 = document.createElement("input");
+  input1.setAttribute("type", "checkbox");
+  li.appendChild(input1);
+  const ul = document.getElementsByTagName("ul")[0];
+  ul.appendChild(li);
+};
